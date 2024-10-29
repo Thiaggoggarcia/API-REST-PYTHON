aaab3e7dabed8a4b5ce0397918f7e9b12d022b91
Tiago Garcia <thiaggoggarcia@gmail.com>
2024-10-29 17:37:59 -0300

    Criando as Rotas de GET e POST

--- app.py
+++ app.py
@@ -1,14 +1,35 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from models.task import Task
 
 app = Flask(__name__)
+task_id_control = 1
+tasks = []
 
-@app.route("/")
-def hello_world():
-    return "Hello World"
+@app.route("/tasks", methods=['POST'])
+def create_tasks():
+    global task_id_control
+    data = request.get_json()
+    new_task = Task(id=task_id_control ,title=data["title"], description=data.get("description",""))
+    tasks.append(new_task)
+    task_id_control += 1
+    return jsonify({"Mensagem": "Nova tarefa criada com sucesso!"})
 
-@app.route("/about")
-def about():
-    return "About Python"   
+
+@app.route("/tasks", methods=['GET'])
+def get_tasks():
+    task_list = []
+    for task in tasks:
+        task_list.append(task.to_dict()) 
+    
+    total_tasks = len(task_list)
+    output = {
+        "tasks":task_list,
+        "total_tasks": total_tasks
+    }
+    return jsonify(output)
 
 if __name__ == "__main__":
     app.run(port=5001,debug=True)
+    
+    
+    
