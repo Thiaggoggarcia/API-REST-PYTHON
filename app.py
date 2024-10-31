90e5d1629a39400d3bf9a45f1c69f6843b2fe5eb
Tiago Garcia <thiaggoggarcia@gmail.com>
2024-10-31 15:51:02 -0300

    Adicionando as funções de Updade e Delete da API

--- app.py
+++ app.py
@@ -17,16 +17,54 @@ def create_tasks():
 
 @app.route("/tasks", methods=['GET'])
 def get_tasks():
-    task_list = []
-    for task in tasks:
-        task_list.append(task.to_dict()) 
     
-    total_tasks = len(task_list)
+    task_list = [task.to_dict() for task in tasks]
     output = {
         "tasks":task_list,
-        "total_tasks": total_tasks
+        "total_tasks": len(task_list)
     }
     return jsonify(output)
+
+@app.route("/tasks/<int:id_task>", methods=['GET'])
+def get_id_task(id_task):
+    if tasks == []:
+        return jsonify({'Mensagem': 'Não Existem Atividades Cadastradas!'}), 404
+    
+    for task in tasks:
+        if task.id == id_task:
+            return task.to_dict()
+
+        return jsonify({"Mensagem": "Atividade Não encontrada!"}), 404
+    
+@app.route("/tasks/<int:taskId>", methods=['PUT'])
+def update_task(taskId):
+    if tasks == []:
+        return jsonify({'Mensagem': 'Não Existem Atividades Cadastradas!'}), 404
+    
+    for task in tasks:
+        print(task.id,type(task.id))
+        if task.id == taskId:
+            data = request.get_json()
+            task.title = data['title']
+            task.description = data['description']
+            task.completed = data['completed']
+            return jsonify({'Mensagem': 'Atividade Atualizada com Sucesso!'})
+        
+        return jsonify({"Mensagem": "Atividade Não encontrada!"}), 404
+            
+@app.route("/tasks/<int:id_task>", methods=['DELETE'])
+def delete_task(id_task):
+    if tasks == []:
+        return jsonify({'Mensagem': 'Não Existem Atividades Cadastradas!'}), 404
+    
+    for task in tasks:
+        if task.id == id_task:
+            print(task)
+            tasks.remove(task)
+            return jsonify({"Mensagem": "Atividade Deletada com Sucesso!"})
+        
+        return jsonify({"Mensagem": "Atividade Não encontrada!"}), 404
+
 
 if __name__ == "__main__":
     app.run(port=5001,debug=True)
